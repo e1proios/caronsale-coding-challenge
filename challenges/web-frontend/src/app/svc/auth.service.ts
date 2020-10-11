eb37620c1c0ed1d8b7d9eaa4f0bdd3d5cc09871f
Ivan Proios <ivan.proios@gmail.com>
2020-10-11 18:28:34 +0200

    getting auth token

--- challenges/web-frontend/src/app/svc/auth.service.ts
+++ challenges/web-frontend/src/app/svc/auth.service.ts
@@ -5,10 +5,13 @@ import {
   HttpErrorResponse } from '@angular/common/http';
 
 import { sha512 } from 'js-sha512';
+import { throwError, BehaviorSubject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { LoginCredentials } from '../model/login-credentials';
 
 const HASH_ITERATIONS = 5;
+const AUTH_ENDPOINT = 'https://caronsale-backend-service-dev.herokuapp.com/api/v1/authentication/';
 
 @Injectable({
   providedIn: 'root'
@@ -29,12 +32,24 @@ export class AuthService {
     }
     return hash;
   }
+  private redirectOnAuthError(error: HttpErrorResponse) {
+    // TODO
+    return throwError(error.error || "authentication error");
+  }
 
   public login(credentials: LoginCredentials) {
     const encryptedPswd = this.hashPassword(credentials.password, HASH_ITERATIONS);
-    return encryptedPswd;
-  }
-  public getAuthToken() {
+    let headers = new HttpHeaders().
+      append('Accept', 'application/json').
+      append('Content-Type', 'application/json');
 
+    this.http.put(
+      `${AUTH_ENDPOINT}${credentials.email}`,
+      { "password": encryptedPswd, "meta": "string" },
+      { headers: headers})
+    .pipe(catchError(this.redirectOnAuthError))
+    .subscribe( response => {
+      console.log(response);
+    });
   }
 }
