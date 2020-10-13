90f6c131fca2f12cca4add2faa3706d0b9de6dca
Ivan Proios <ivan.proios@gmail.com>
2020-10-13 20:46:33 +0200

    auctions

--- challenges/web-frontend/src/app/components/auctions/auction/auction.component.ts
+++ challenges/web-frontend/src/app/components/auctions/auction/auction.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-auction',
   templateUrl: './auction.component.html',
@@ -7,8 +9,29 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class AuctionComponent implements OnInit {
   @Input() auctionData;
+  carInfo;
+  auctionInfo;
 
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const car = this.auctionData.associatedVehicle;
+
+    this.carInfo = {
+      type: `${car.make} ${car.model}`,
+      color: car.color,
+      ez: car.ez,
+      km: `${car.mileageInKm} km`,
+      fuelType: car.fuelType,
+      transmission: car.transmission
+    };
+    this.auctionInfo = {
+      highestBid: this.auctionData.currentHighestBidValue,
+      highestBidderId: this.auctionData._fk_highestBiddingSalesmanUser,
+      remainingTime: this.formatTime(this.auctionData.remainingTimeInSeconds)
+    }
+  }
+  private formatTime(secs: number): string {
+    return moment.utc(secs*1000).format('HH:mm:ss');
+  }
 }
